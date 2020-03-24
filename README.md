2f9cee13c8b18572063f9832d5e70c1b2d0c5782
Natale Vinto <nvinto@redhat.com>
2020-03-24 02:36:39 +0100

    Adding first codebase

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # smartqueue
 Esselungaacasa.it smart free delivery slots alerting system
+
+
+## Uso
+
+```
+$ ./esselunga.pl -u username -p password
+$ ./esselunga.pl -u username -p password -e email@test.com,email1@test.com,email2.test.com
+$ USERNAME=username PASSWORD=password EMAIL=email@test.com ./esselunga.pl
+```
---
+++ esselunga.pl
@@ -0,0 +1,296 @@
+#!/usr/bin/perl -w
+############################################################################
+#    Esselunga Slot checker                                                #
+#    Copyright (C) 2020 Natale Vinto                                       #
+#    ebballon@gmail.com                                                    #
+#                                                                          #
+#    This program is free software; you can redistribute it and#or modify  #
+#    it under the terms of the GNU General Public License as published by  #
+#    the Free Software Foundation; either version 2 of the License, or     #
+#    (at your option) any later version.                                   #
+#                                                                          #
+#    This program is distributed in the hope that it will be useful,       #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#    GNU General Public License for more details.                          #
+#                                                                          #
+#    You should have received a copy of the GNU General Public License     #
+#    along with this program; if not, write to the                         #
+#    Free Software Foundation, Inc.,                                       #
+#    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
+############################################################################
+
+
+use strict;
+use warnings;
+
+use feature 'say';
+
+use LWP::UserAgent;
+use HTTP::Request::Common qw(POST);
+use HTTP::Cookies;
+use HTML::Entities;
+use Getopt::Std;
+use Encode;
+use MIME::Lite;
+use MIME::Base64;
+use Authen::SASL;
+use DBI;
+
+
+
+my $DEBUG = 1;
+my $smtp_server = 'mail.tresud.it';
+my $smtp_user = 'tresud.it';
+my $smtp_pass = 'womangaw';
+my $smtp_port = '587';
+
+
+my %options = ();
+my $username = '';
+my $password = '';
+my $email = '';
+
+if ($ENV{USERNAME} && $ENV{PASSWORD}) {
+
+  $username = $ENV{USERNAME};
+  $password = $ENV{PASSWORD};
+  
+  if ($ENV{EMAIL}){
+    $email = $ENV{EMAIL}; 
+  }
+} else {
+    getopt( 'upe', \%options );
+
+    if ( !$options{u} || !$options{p} ) {
+        say "Opzioni: -u USERNAME -p PASSWORD [-e EMAIL]\n";
+        say "Esempio: esselunga.pl -u esempio\@gmail.com -p password";
+        exit 1;
+    }
+    $username = $options{u};
+    $password = $options{p};
+    if ($options{e}){
+        $email = $options{e};
+    }
+}
+
+my $dbfile = "esselunga.sqlite";
+my $dbh =
+    DBI->connect( "dbi:SQLite:dbname=$dbfile", "", "",
+    { RaiseError => 1 } )
+or die $DBI::errstr;
+
+
+my $stmt = qq(CREATE TABLE IF NOT EXISTS slots
+(   start_time  DATETIME NOT NULL UNIQUE,
+    email_sent    INT     NOT NULL,
+    text          VARCHAR(50)););
+
+my $rv = $dbh->do($stmt);
+if($rv < 0) {
+    say $DBI::errstr;
+} else {
+    say "Database structure Ready";
+}
+
+
+my $url =
+'https://www.esselunga.it/area-utenti/applicationCheck?appName=esselungaEcommerce&daru=https%3A%2F%2Fwww.esselungaacasa.it%3A443%2Fecommerce%2Fnav%2Fauth%2Fsupermercato%2Fhome.html%3F&loginType=light';
+
+
+sub send_mail {
+    my ($emails,$body) = @_;
+    my @emails = split(/,/, $emails);
+    my $to = $emails[0];
+    my $cc = '';
+    for (my $i = 1; $i < scalar(@emails); $i++){ 
+        $cc.="$emails[$i],";
+    }
+    my $from = 'info@tresud.it';
+    my $subject = 'Slot disponibili';
+    my $message = $body;
+    
+    
+    
+    my $msg = MIME::Lite->new(
+                    From     => $from,
+                    To       => \@emails,
+                    Type     => 'TEXT',
+                    Cc       => $cc,
+                    Subject  => $subject,
+                    Data     => $message
+                    );
+                    
+    #$msg->attr("content-type" => "text/html");         
+    $msg->send('smtp', $smtp_server, AuthUser=>$smtp_user, AuthPass=>$smtp_pass,  SSL => 1, Port => $smtp_port  );
+    say "Email Inviata!";
+}
+
+
+    
+
+
+
+my $ua        = LWP::UserAgent->new();
+my $cookiejar = HTTP::Cookies->new();
+$ua->cookie_jar($cookiejar);
+$ua->agent('Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.; Trident/5.0)');
+
+my $res     = $ua->get($url);
+my $content = '';
+
+unless ( $res->is_success ) {
+    say "Non posso raggiungere il sito esselunga.it, skipping";
+    exit 1;
+
+}
+
+say "Homepage OK";
+
+$content = $res->content if $DEBUG;
+
+my $csrf = '';
+
+$ua->default_header( 'Referer' =>
+'https://www.esselunga.it/area-utenti/applicationCheck?appName=esselungaEcommerce&daru=https%3A%2F%2Fwww.esselungaacasa.it%3A443%2Fecommerce%2Fnav%2Fauth%2Fsupermercato%2Fhome.html%3F&loginType=light'
+);
+$ua->default_header( 'FETCH-CSRF-TOKEN' => '1' );
+push @{ $ua->requests_redirectable }, 'POST';
+
+$res = POST 'https://www.esselunga.it/area-utenti/csrfjs';
+
+my $resp = $ua->request($res);
+$content = $resp->content;
+
+if ( $content =~ /^X-CSRF-TOKEN:(.*)$/ ) {
+    $csrf = $1;
+}
+else {
+    say "Non posso estrapolare il token CSRF, skipping";
+    say $content if $DEBUG;
+    exit 1;
+}
+
+say "Token: $csrf" if $DEBUG;
+
+$ua->default_header( 'Referer' =>
+'https://www.esselunga.it/area-utenti/applicationCheck?appName=esselungaEcommerce&daru=https%3A%2F%2Fwww.esselungaacasa.it%3A443%2Fecommerce%2Fnav%2Fauth%2Fsupermercato%2Fhome.html%3F&loginType=light'
+);
+
+$res = POST 'https://www.esselunga.it/area-utenti/loginExt',
+  [
+    username => $username,
+    password => $password,
+    daru =>
+'https://www.esselungaacasa.it:443/ecommerce/nav/auth/supermercato/home.html',
+    dare =>
+'https://www.esselunga.it/area-utenti/applicationCheck?appName=esselungaEcommerce&daru=https%3A%2F%2Fwww.esselungaacasa.it%3A443%2Fecommerce%2Fnav%2Fauth%2Fsupermercato%2Fhome.html%3F&loginType=light',
+    appName        => 'esselungaEcommerce',
+    promo          => '',
+    'X-CSRF-TOKEN' => $csrf
+  ];
+
+$resp    = $ua->request($res);
+$content = $resp->content;
+
+unless ( $resp->is_success ) {
+    say "Non posso effettuare il login, skipping";
+    say $content if $DEBUG;
+    exit 1;
+}
+
+say "Login OK";
+my $cookies = $cookiejar->as_string;
+my $xsfr    = '';
+
+if ( $cookies =~ /XSRF-ECOM-TOKEN=(.*?);/ ) {
+    $xsfr = $1;
+    say "XSFR: $xsfr" if $DEBUG;
+}
+
+$ua->default_header( 'Content-Type' => 'application/json' );
+$ua->default_header( 'X-XSRF-TOKEN' => $xsfr );
+
+$res = $ua->get(
+    'https://www.esselungaacasa.it/ecommerce/resources/auth/slot/status');
+$content = $res->content;
+
+unless ( $res->is_success ) {
+    say "Non posso verificare gli slot, skipping";
+    say $content if $DEBUG;
+    exit 1;
+}
+
+my $ok = 0;
+my $slots = '';
+my $html = '';
+while ( $content =~
+/"viewStatus":"(.+?)"(.+?)"endTime":"(.+?)"(.+?)"startTime":"(.+?)","status":"(.+?)"/gi
+  )
+{
+    my $status     = $1;
+    my $start_time = $5;
+    my $end_time   = $3;
+    my $s_status   = $6;
+
+    if (   $status ne 'ESAURITA'
+        && $status ne 'INIBITA'
+        && $s_status ne 'DISABLED' )
+    {
+
+        my $start_time_date = '';
+        my $start_time_hour = '';
+        my $end_time_date = '';
+        my $end_time_hour = '';
+        if ($start_time =~ /(.*)T(.*):00:00.000\+0000/){
+            $start_time_date = $1;
+            $start_time_hour = $2;
+            $start_time_hour+=2;
+        }
+        if ($end_time =~ /(.*)T(.*):00:00.000\+0000/){
+            $end_time_date = $1;
+            $end_time_hour = $2;
+            $end_time_hour+=2;
+        }
+        my $slots = "SLOT: $status - Data Inizio: $start_time_date Ore: $start_time_hour:00,  Data Fine: $end_time_date Ore: $end_time_hour:00";
+        
+        say $slots;
+        $ok = 1;
+        my $sth = $dbh->prepare('UPDATE OR IGNORE slots SET email_sent = 1 WHERE start_time = ?');
+        $sth->execute($start_time) or die $DBI::errstr;
+
+        $sth = $dbh->prepare("INSERT OR IGNORE INTO slots(start_time, email_sent, text) VALUES(?,?,?)");
+        $sth->execute($start_time, 0, $slots) or die $DBI::errstr;
+
+        #$html  .= "<p>SLOT: <b>$status</b> - Inizio: "
+    }
+
+}
+
+say "Check slot completo";
+if ($ok) {
+    say "BINGO!";
+    
+    my $sth = $dbh->prepare("SELECT text FROM slots WHERE email_sent = 0")
+            or die "prepare statement failed: $dbh->errstr()";
+    
+    $sth->execute() or die "execution failed: $dbh->errstr()"; 
+ 
+    my $text;
+    my $message = '';
+    while($text = $sth->fetchrow()){
+        $message .= "$text\n";                   
+    }
+    
+    my $send_text = "Ciao,\nSono liberi degli slot:\n\n" . $message . "\nBuona spesa!\n";
+    
+    
+    if ($email ne ''){
+            send_mail($email, $send_text);
+    }
+
+} else {
+    say "Nessun nuovo slot disponibile, riprovare";
+}
+
+
